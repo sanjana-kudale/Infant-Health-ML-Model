42e6040bf22a8bda152b913c4d916bc23a2c2dde
sanjana-kudale <166540136+sanjana-kudale@users.noreply.github.com>
2025-01-30 18:08:59 +0530

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -1,7 +1,15 @@
-import os
-os.chdir(r"C:\Users\HP\jupyterNotebook\collage")
-
 import streamlit as st
 
 st.title("My First Streamlit App")
 st.write("Hello, welcome to my Streamlit app!")
+
+df = pd.read_csv("Synthetic-Infant-Health-Data.csv")
+
+import streamlit as st
+import pandas as pd
+
+uploaded_file = st.file_uploader("Synthetic-Infant-Health-Data", type="csv")
+
+if uploaded_file is not None:
+    df = pd.read_csv('Synthetic-Infant-Health-Data.csv')
+    st.write(df)
