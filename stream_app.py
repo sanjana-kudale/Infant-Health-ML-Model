4703e721c18fe6b291523b58d54bbf3908a40827
sanjana-kudale <166540136+sanjana-kudale@users.noreply.github.com>
2025-01-30 18:15:50 +0530

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import streamlit as st
 import pandas as pd
+import joblib
 
 st.title("My First Streamlit App")
 st.write("Hello, welcome to my Streamlit app!")
@@ -12,3 +13,6 @@ uploaded_file = st.file_uploader("Synthetic-Infant-Health-Data", type="csv")
 if uploaded_file is not None:
     df = pd.read_csv('Synthetic-Infant-Health-Data.csv')
     st.write(df)
+
+grid = joblib.load("rf_classifier.pkl")
+grid.best_estimator_
