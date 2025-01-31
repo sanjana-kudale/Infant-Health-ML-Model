09daea72ba94d135fa646feb6739b7808d5e55ce
sanjana-kudale <166540136+sanjana-kudale@users.noreply.github.com>
2025-01-31 12:29:48 +0530

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 import joblib
 
 st.title("My First Streamlit App")
