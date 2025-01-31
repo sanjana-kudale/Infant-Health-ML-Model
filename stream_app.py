15717a4021f02f41005f8469c77aed2b6281d11b
sanjana-kudale <166540136+sanjana-kudale@users.noreply.github.com>
2025-01-31 12:16:50 +0530

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -8,7 +8,7 @@ st.write("Hello, welcome to my Streamlit app!")
 
 df = pd.read_csv("Synthetic-Infant-Health-Data.csv")
 
-uploaded_file = st.file_uploader("Synthetic-Infant-Health-Data", type="csv")
+uploaded_file = st.file_uploader("rf_classifier, type="ipynb")
 
 if uploaded_file is not None:
     df = pd.read_csv('Synthetic-Infant-Health-Data.csv')
